a22494e427c09d52858f6982bf08dae388a1d02b
King Songtao <king.songtao@gmail.com>
2024-12-13 01:00:43 +0800

    requirements

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ def receipt_preview_page(output_doc, receipt_filename):
     """
     收据预览页面
     """
-    st.title('💻ATM Receipt')
+    st.title('🧾ATM Receipt')
     st.success(f"收据创建成功！", icon="✅")
     st.info('点击"下载收据"按钮即可下载Word收据')
 
@@ -131,7 +131,7 @@ def main_page():
     """
     主页面
     """
-    st.title('💻ATM Receipt')
+    st.title('🧾ATM Receipt')
 
     st.divider()
 
