c2295e8b3e6cfb7ab3977dd24e8a5a31ed996383
King Songtao <king.songtao@gmail.com>
2024-12-13 16:38:11 +0800

    requirements

--- app.py
+++ app.py
@@ -327,8 +327,13 @@ def quotation_page():
     """
     st.title('💰Money Maker')
     st.divider()
+    class_date = st.date_input("授课日期：")
+    class_name = st.text_input("课程名称：")
     st.info("请将通义听悟中的章节概览粘贴到下面的输入框中。", icon="ℹ️")
     section_review = st.text_area("")
+
+    if st.button('生成ChatGPT提示词', use_container_width=True, type='primary'):
+        system_prompt = "你现在是一个拥有三十年教学经验的初中英语老师，你刚刚完成一节英语课的授课，以下是记录的课堂授课内容章节速览：\n\n"
 
 
 def main():
